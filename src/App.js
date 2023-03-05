b238f09598aee8a83ee7b1fa173c552ce5f8981f
Muhammad Farooq Chaudhry <110221241+FarooqMangoTech@users.noreply.github.com>
2023-03-06 01:22:02 +0500

    MUI setup with initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Main from './components/Main';
+import Appbar from './components/Appbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Switch, Typography } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import SocialLinksWidget from './components/SocialLinksWidget';
 
-function App() {
+export default function App() {
+  const [theme, settheme] = useState(false);
+
+  //insi de this, we can override any object's value with our own.
+  const customTheme = createTheme({
+    palette: {
+      mode: theme ? 'dark' : 'light',
+    },
+  });
+
+  const handleChange = (event) => {
+    settheme(event.target.checked);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Main></Main>
+        <Appbar></Appbar>
+        <Typography>
+          Switch to Dark mode?
+          <Switch checked={theme} color='success' onChange={handleChange} />
+        </Typography>
+        <SocialLinksWidget />
+      </ThemeProvider>
     </div>
   );
 }
-
-export default App;
---
+++ src/customization/customThemeOptions.js
@@ -0,0 +1,12 @@
+//inside this, we can override any object's value with our own.
+export const customThemeOptions = {
+  pallete: {
+    type: 'dark',
+    // primary: {
+    //   //main: ''
+    // },
+    // secondary: {
+    //   //main: ''
+    // },
+  },
+};
